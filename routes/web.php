97a986654ee23224cde0c2445429a0a8921978bc
jiro.kirameki <ec2-user@ip-172-31-10-198.us-east-2.compute.internal>
2018-06-05 04:15:20 +0000

    kin

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,9 @@ Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
-Route::get('/', function () {
-    return view('welcome');
-    
-});
+//Route::get('/', function () {
+ //   return view('welcome');
+//});
 
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
